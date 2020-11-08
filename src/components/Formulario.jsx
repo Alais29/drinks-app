9147a23b8aa04c97d59424904232bbf6afbef96d
Alfonsina <alfonsina.l@gmail.com>
2020-11-08 16:56:10 -0300

    First commit. Form layout done

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const Formulario = () => {
+  return (
+    <form
+      className="col-12"
+    >
+      <fieldset className="text-center">
+        <legend>Busca bebidas por Categoría o Ingrediente</legend>
+      </fieldset>
+      <div className="row mt-4">
+        <div className="col-md-4">
+          <input
+            type="text"
+            name="nombre"
+            className="form-control"
+            placeholder="Buscar por Ingrediente"
+          />
+        </div>
+        <div className="col-md-4">
+          <select
+            className="form-control"
+            name="categoria"
+          >
+            <option value="">-- Selecciona Categoría --</option>
+          </select>
+        </div>
+        <div className="col-md-4">
+          <input
+            type="submit"
+            className="btn btn-block btn-primary"
+            value="Buscar Bebidas"
+          />
+        </div>
+      </div>
+    </form>
+  )
+}
+
+export default Formulario
